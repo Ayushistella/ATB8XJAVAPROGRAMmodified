a18db31814ab826ada4690294333f8fe884e3871
Ayushistella <ayushisrivastava12as@gmail.com>
2024-09-29 14:18:31 +0530

    20septassignment

---
+++ src/Sept/ex_20092024/lab001.java
@@ -0,0 +1,11 @@
+package Sept.ex_20092024;
+
+public class lab001 {
+    public static void main(String[] args) {
+        int score= 75;
+        String grade= score<=60 ? "C":score>=80? "A": "B";
+        System.out.println(grade);
+
+
+    }
+}
